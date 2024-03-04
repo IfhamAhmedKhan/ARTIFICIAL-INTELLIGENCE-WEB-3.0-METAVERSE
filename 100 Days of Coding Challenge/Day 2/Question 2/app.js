a598a6df1aa0f758f6cdb46d2f90562992907283
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-04 22:40:55 +0500

    day 2
    day 2 of coding challenge completed

---
+++ 100 Days of Coding Challenge/Day 2/Question 2/app.js
@@ -0,0 +1,4 @@
+//Question 2: Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var author = "Oprah Winfrey";
+var quote = "“Think like a queen. A queen is not afraid to fail. Failure is another steppingstone to greatness”";
+console.log(author + "once said, " + quote);
