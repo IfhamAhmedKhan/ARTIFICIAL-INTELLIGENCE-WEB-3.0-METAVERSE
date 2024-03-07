dad8a06e5d3dad659ed498b4f50f57cc2b8d8544
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-07 14:12:49 +0500

    Day 6
    Day 6 of coding challenge completed

---
+++ 100 Days of Coding Challenge/Day 6/Question 1/src/app.ts
@@ -0,0 +1,13 @@
+// Question 16: More Guests: You've found a bigger dinner table, so there's room for more guests.
+
+//previous array
+let guests: string[] = ["Aayan", "Afnan", "Farman"];
+console.log("Great news! I found a bigger dinner table!");
+
+guests.unshift("Ifham");
+guests.splice(guests.length / 2, 0, "Sana");
+guests.push("Sahar");
+
+for (let index = 0; index < guests.length; index++) {
+    console.log(`Dear ${guests[index]}, would you like to join me for dinner?`);    
+}
