d294570fd56db48ad7eb13663c613bf2f07cf003
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-10 12:15:59 +0500

    Day 8
    Day 8 task completed

---
+++ 100 Days of Coding Challenge/Day 8/Question 2/dist/app.js
@@ -0,0 +1,25 @@
+"use strict";
+// Question 23: Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+let car = ["BMW", "Toyota", "Honda", "Ford", "Chevrolet"];
+// True tests
+console.log("Is car[0] == 'BMW'? I predict True.");
+console.log(car[0] == 'BMW'); // True
+console.log("Is car[1] == 'Toyota'? I predict True.");
+console.log(car[1] == 'Toyota'); // True
+console.log("Is car[2] == 'Honda'? I predict True.");
+console.log(car[2] == 'Honda'); // True
+console.log("Is car[3] == 'Ford'? I predict True.");
+console.log(car[3] == 'Ford'); // True
+console.log("Is car[4] == 'Chevrolet'? I predict True.");
+console.log(car[4] == 'Chevrolet'); // True
+// False tests
+console.log("Is car[0] == 'Toyota'? I predict False.");
+console.log(car[0] == 'Toyota'); // False
+console.log("Is car[1] == 'Honda'? I predict False.");
+console.log(car[1] == 'Honda'); // False
+console.log("Is car[2] == 'Ford'? I predict False.");
+console.log(car[2] == 'Ford'); // False
+console.log("Is car[3] == 'Chevrolet'? I predict False.");
+console.log(car[3] == 'Chevrolet'); // False
+console.log("Is car[4] == 'BMW'? I predict False.");
+console.log(car[4] == 'BMW'); // False
