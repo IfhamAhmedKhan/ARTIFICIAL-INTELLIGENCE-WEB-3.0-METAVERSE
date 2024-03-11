290603b8dcaf090dff1d0a47c3dddd7c6dda452e
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-11 12:18:39 +0500

    Day 9
    day 9 of coding challenge completed

---
+++ 100 Days of Coding Challenge/Day 9/Question 2/src/app.ts
@@ -0,0 +1,10 @@
+// Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+
+let alien_color:string = "green"
+
+if (alien_color != "green"){
+    console.log("Incorrect sorry you lost your life, Now Die (Gun shot!)")
+}
+else {
+    console.log("Congrats you just earn 10 points!")
+}
---
+++ 100 Days of Coding Challenge/Day 9/Question 3/dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+// Question 27: Alien Colors #3: Convert your if-else chain to handle three colors: green, yellow, red.
+let alien_color = "purple";
+if (alien_color == "green") {
+    console.log("You earned 10 points for guessing green.");
+}
+else if (alien_color == "yellow") {
+    console.log("You earned 10 points for guessing yellow.");
+}
+else if (alien_color == "red") {
+    console.log("You earned 10 points for guessing red.");
+}
+else {
+    console.log(`You are not smart! You guessed ${alien_color} seriously?`);
+}
