0ea782b700c4a40335a5746c84bef900bf9fe317
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-15 12:08:42 +0500

    Day 13
    Day 13 task completed

--- 100 Days of Coding Challenge/Day 13/Question 1/src/app.ts
+++ 100 Days of Coding Challenge/Day 13/Question 1/src/app.ts
@@ -1,1 +1,9 @@
 // Question 37: Large Shirts: Default values in make_shirt().
+
+function make_shirt(color: string = "red", size: string = "medium", message: string = "I like coding") {
+    console.log(`${color} Color shirt size is ${size} and a message "${message}" on the front side.`);
+}
+
+make_shirt();
+make_shirt("blue", "small");
+make_shirt("black", "large", "I never quit!"); 
--- 100 Days of Coding Challenge/Day 13/Question 2/dist/app.js
+++ 100 Days of Coding Challenge/Day 13/Question 2/dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
 // Question 38: Cities: Describing cities with a function.
+function describe_city(city, country = "Pakistan") {
+    console.log(`${city} is in ${country}.`);
+}
+describe_city("Karachi");
+describe_city("Tokyo", "Japan");
+describe_city("Berlin", "Germany");
+describe_city("Auckland", "New zealand");
