cca6fdc1aaee345c0549541e856187d47f749f74
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-18 11:12:26 +0500

    structure for day 16

---
+++ 100 Days of Coding Challenge/Day 16/Question 1/dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// Question 46: Enhanced Laptop Object: Construct an object for a laptop including properties make, model, year, and a method describe() that logs a sentence about the laptop.
+let laptop = {
+    make: "Dell",
+    model: "vPro",
+    year: 2022,
+    describe: function () {
+        console.log(`Laptop details: Make: ${this.make}, Model: ${this.model}, Year: ${this.year}.`);
+    }
+};
+laptop.describe();
