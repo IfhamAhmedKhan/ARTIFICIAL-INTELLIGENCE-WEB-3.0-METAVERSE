51cb2dfe72fbbd5e961a5add969df1e1afbbd7d7
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-19 21:05:55 +0500

    day 16
    question 2 solved

--- 100 Days of Coding Challenge/Day 16/Question 2/dist/app.js
+++ 100 Days of Coding Challenge/Day 16/Question 2/dist/app.js
@@ -1,5 +1,5 @@
+"use strict";
 // Question 47: Advanced Array Destructuring: Given an array of objects representing different laptops, each with properties make, model, and year, use array destructuring to assign the first and second laptops to variables. Then, log these variables.
-
 let laptops = [
     { make: "Dell", model: "XPS 15", year: 2021 },
     { make: "Apple", model: "MacBook Pro", year: 2020 },
