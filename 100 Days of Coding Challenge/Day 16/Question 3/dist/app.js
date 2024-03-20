6c5e54c1bc44abd0da7a1736eea692bbcb412ba8
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-20 13:15:56 +0500

    finally day 16

--- 100 Days of Coding Challenge/Day 16/Question 3/dist/app.js
+++ 100 Days of Coding Challenge/Day 16/Question 3/dist/app.js
@@ -1,5 +1,5 @@
+"use strict";
 // Question 48: Combining Arrays with Spread Operator: Suppose you're comparing prices of two different sets of laptops. Use the spread operator to combine these arrays into a single array sorted in ascending order, then log the result.
-
 let pricesSet1 = [1500, 1800, 2500];
 let pricesSet2 = [900, 5500, 3000];
 let combinedPrices = [...pricesSet1, ...pricesSet2].sort((a, b) => a - b);
