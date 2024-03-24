627202565b985e3e0310fa1f86fcdad1b5c915cc
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-24 16:04:35 +0500

    question 5
    assignment

---
+++ Assignments/45 Assignments Questions/Question 5/dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let author = "Oprah Winfrey";
+let quote = "“Think like a queen. A queen is not afraid to fail. Failure is another steppingstone to greatness”";
+console.log(author + "once said, " + quote);
