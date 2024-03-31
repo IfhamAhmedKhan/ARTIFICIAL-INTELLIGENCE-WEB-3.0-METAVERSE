309274c41bf433cd5ba9cbf51ed9863f03d3da56
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-03-31 22:39:13 +0500

    practice problem

--- Python/1. Basic/SoloLearn.py
+++ Python/1. Basic/SoloLearn.py
@@ -9,3 +9,11 @@ scores = [43,75,88,443,857,97]
 high_scores = [scores for scores in scores if scores > 80]
 
 print(high_scores)
+
+#------------------------------------
+
+words = ["tree", "button", "cat", "window", "defenestrate"]
+
+longer_list = [x for x in words if len(x) > 4]
+
+print(longer_list)
