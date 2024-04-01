a232bd8ed7c6483013af0041b6faa439d8afa390
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-04-01 10:48:01 +0500

    try/except/raise/else

--- Python/1. Basic/SoloLearn.py
+++ Python/1. Basic/SoloLearn.py
@@ -1,19 +1,57 @@
-sports = ["football", "Basketball", "Tennis"]
+# sports = ["football", "Basketball", "Tennis"]
 
-group = [x for x in sports if "ball" in x]
+# group = [x for x in sports if "ball" in x]
 
-print(group)
+# print(group)
 
-scores = [43,75,88,443,857,97]
+# scores = [43,75,88,443,857,97]
 
-high_scores = [scores for scores in scores if scores > 80]
+# high_scores = [scores for scores in scores if scores > 80]
 
-print(high_scores)
+# print(high_scores)
 
-#------------------------------------
+# #------------------------------------
 
-words = ["tree", "button", "cat", "window", "defenestrate"]
+# words = ["tree", "button", "cat", "window", "defenestrate"]
 
-longer_list = [x for x in words if len(x) > 4]
+# longer_list = [x for x in words if len(x) > 4]
 
-print(longer_list)
+# print(longer_list)
+
+# # -----------------
+# # Exception
+
+# prices = [250, 300, "240", 400]
+# try:
+#   total = sum(prices)
+#   print(total)
+# except TypeError:
+#   print("Invalid data type")
+
+# print("Happy Shopping")
+
+# my example
+# numberOne = 10
+# numberTwo = 0
+# try:
+#     total = numberOne/numberTwo
+
+# except ZeroDivisionError:
+#     print("please don't divide by zero")
+
+
+
+# print(numberOne+numberTwo)
+
+# example ->
+# products = ['ball', 'toy', 'paper']
+# try:
+#   count = len(products)
+# except:
+#  print("Error")
+# else:
+#   print("Count of products:", count)
+
+# numb = [1,2,3]
+# print(numb[10])
+
