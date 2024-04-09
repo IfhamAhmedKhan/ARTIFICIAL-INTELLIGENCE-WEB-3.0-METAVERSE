1a0c2a06f52a5b833cb942e0edf9afc3cf63f986
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-04-09 22:23:13 +0500

    basic

---
+++ Python/1. Basic/aayan.py
@@ -0,0 +1,13 @@
+name=input("Enter your name:")
+
+number=int(input("Enter your age:"))
+#taking input
+
+#Creating output
+if (number>=18):
+    print(name,"you are able to vote")
+elif (number<18):
+    print(name,"you are not able to vote")
+else:
+    print("Please put the right details")
+#Output is generated
